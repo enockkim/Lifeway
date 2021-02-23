1e2a3f0d5a56ad7d396f6c2e930c3a20106eb9aa
Enock <Enock@DESKTOP-862AS8S>
2021-02-23 05:47:19 +0300

    backup

--- Lifeway/Controllers/FinanceController.cs
+++ Lifeway/Controllers/FinanceController.cs
@@ -36,19 +36,41 @@ namespace Lifeway.Controllers
             Console.WriteLine(response.Content);
 
 
-            var _dataResponse = JToken.Parse(JsonConvert.SerializeObject(response.Content));
+            //var _dataResponse = JToken.Parse(JsonConvert.erializeObject(response.Content));
             //var access_token = _dataResponse["access_token"];
+            var tkn = JsonConvert.DeserializeObject<dynamic>(response.Content);
+            string a_t = (tkn.ToString(Formatting.None));
+            JObject at = JObject.Parse(a_t);
+            string a_to = (string)at.SelectToken("access_token");
 
-            //MpesaApiConfiguration api = new MpesaApiConfiguration
-            //{
-            //    token = access_token
-            //};
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            MpesaApiConfiguration[] persons = js.Deserialize<MpesaApiConfiguration[]>(access_token);
+            var balance_client = new RestClient("https://api.safaricom.co.ke/mpesa/accountbalance/v1/query");
+            client.Timeout = -1;
+            var balance_request = new RestRequest(Method.POST);
+            balance_request.AddHeader("Authorization", "Bearer "+a_to);
+            balance_request.AddHeader("Content-Type", "application/json");
+            balance_request.AddParameter("application/json", "{\r\n    \"Initiator\": \"Enock\",\r\n    \"SecurityCredential\": \"HToNsCLwNvvKMZXInHKmgn4tHjx8PNWx9zcpfw3zZc7Ss2odSR5xXCjmISB0zxchKU9zUPAMC0OawkHRD5YDP9fQ7db6UIWQuIpYJdqo/IdwowghUr8OcYwbtf6u4BpNPfHxxzgQFSSH6RnmwLbUYurzizufxMlQf4//IB+VfV7uHyh0x4iWLxHeUcaDamafluQ5iifcXR52mYs03vY31A6moo7V3/bydCRezrxaj8Ao2Zsns6Tsas457o8x1tNhtVTBnK9aDkAeO+y8ezCoys5R1l5kFmBuvhQ+aQD9y/jVpZGkRIWmMnT+xVoe5HVgxjj743T6HPo39f2wCervag==\",\r\n    \"CommandID\": \"AccountBalance\",\r\n    \"PartyA\": \"994805\",\r\n    \"IdentifierType\": \"4\",\r\n    \"Remarks\": \"check\",\r\n    \"QueueTimeOutURL\": \"http://portal.lifewaychristianacademy.sc.ke/paybill/confirmation.php\",\r\n    \"ResultURL\": \"http://portal.lifewaychristianacademy.sc.ke/paybill/confirmation.php\"\r\n}", ParameterType.RequestBody);
+            IRestResponse balance_response = balance_client.Execute(balance_request);
+            Console.WriteLine(balance_response.Content);
 
-            var token = JObject.Parse(response.Content);
+            var balance = JsonConvert.DeserializeObject<dynamic>(balance_response.Content);
+            string sbalance = (balance.ToString(Formatting.None));
+            JObject obalance = JObject.Parse(sbalance);
+            string sebalance = (string)obalance.SelectToken("balance");
 
-            return View(_dataResponse);
+            //MpesaApiConfiguration a_token = ((JArray)a_t.access_token)[0].ToObject<MpesaApiConfiguration>();
+
+            MpesaApiConfiguration api = new MpesaApiConfiguration
+            {
+                token = sebalance
+            };
+            //JavaScriptSerializer js = new JavaScriptSerializer();
+            //MpesaApiConfiguration[] persons = js.Deserialize<MpesaApiConfiguration[]>(access_token);
+
+            //var token = JObject.Parse(response.Content);
+
+
+
+            return View(api);
         }
 
 
